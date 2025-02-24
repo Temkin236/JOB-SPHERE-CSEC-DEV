4e5ca99f8c82f04b73f2da0435934843bd3f80ca
Temkin236 <temkinabdulmelik1@gmail.com>
2025-02-24 02:55:40 -0800

    Update App.jsx

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Description from './Components/Description.jsx';
-import ApplicationForm from './Components/ApplicationForm.jsx';
-import Login from './Authentication/Login.jsx';
-import Signup from './Authentication/Signup.jsx';
-import Home from './Components/Home.jsx';
+import Description from '../Components/Description.jsx';
+import ApplicationForm from './ApplicationForm.jsx';
+import Login from '../Authentication/Login.jsx';
+import Signup from '../Authentication/Signup.jsx';
+import Home from '../Components/Home.jsx';
 
 const App = () => {
   return (
@@ -15,6 +15,7 @@ const App = () => {
         <Route path="/apply" element={<ApplicationForm />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/posting" element={<Posting />} />
       </Routes>
     </Router>
   );
