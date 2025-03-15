14ef7f1e68806ccf867dae84bba54cc99bda8555
Temkin236 <temkinabdulmelik1@gmail.com>
2025-03-15 13:57:39 +0300

    Delete Backend directory

--- Backend/src/middleware/logger.js
+++
@@ -1,7 +0,0 @@
-const logger = (req, res, next) => {
-  const timestamp = new Date().toISOString();
-  console.log(`[${timestamp}] ${req.method} ${req.path}`);
-  next();
-};
-
-export default logger;
